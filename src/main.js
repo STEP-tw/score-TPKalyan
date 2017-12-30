55beacfa9beab749184f2af9a3cb17d2b340151a
Pavan kalyan <pavankalyanthota@gmail.com>
2017-12-30 14:48:43 +0530

    score will increase if the snake ate the food but displaying score is pending

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ let snake=undefined;
 let food=undefined;
 let numberOfRows=60;
 let numberOfCols=120;
+let score = 0;
 
 let animator=undefined;
 
@@ -13,10 +14,16 @@ const animateSnake=function() {
   unpaintSnake(oldTail);
   paintHead(head);
   if(head.isSameCoordAs(food)) {
+    increaseScore();
+    console.log(score);
     snake.grow();
     createFood(numberOfRows,numberOfCols);
     drawFood(food);
   }
+}
+
+const increaseScore = function(){
+  score += 10;
 }
 
 const changeSnakeDirection=function(event) {
